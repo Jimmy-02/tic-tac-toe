7eef9e4bba5b558a3a8553f91c2658849737b167
Jimmy-02 <tainat1202@gmail.com>
2024-06-10 13:50:28 +0700

    done gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,26 +6,32 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard() {
-    const [GameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-            updatedBoard[rowIndex][colIndex] = 'X';
-            return updatedBoard;
-        });
-    }
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+        return updatedBoard;
+    });
 
-    return <ol id="game-board">
-        {GameBoard.map((row, rowIndex) => <li key={rowIndex}>
-            <ol>
-                {row.map((playerSymbol, colIndex) => (
-                    <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
-                    </li>
-                ))}
-            </ol>
-        </li>)}
+      onSelectSquare();
+  }
+
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+          </li>
+      ))}
     </ol>
+  );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
@@ -13,18 +13,22 @@ export default function Player({ initialName, symbol }) {
     }
 
     let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let btnCaption = 'Edit';
 
     if (isEditing) {
-        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
-    }
+      editablePlayerName = (
+          <input type="text" required value={playerName} onChange={handleChange} />
+      );
+      // btnCaption = 'Save';
+  }
 
     return (
-        <li>
-            <span className="player">
-                {editablePlayerName}
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
-        </li>
-    )
+      <li className={isActive ? 'active' : undefined}>
+          <span className="player">
+              {editablePlayerName}
+              <span className="player-symbol">{symbol}</span>
+          </span>
+          <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      </li>
+  );
 }
